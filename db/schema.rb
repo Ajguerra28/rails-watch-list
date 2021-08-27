9336bb0426d1279d5bd481205b80249e2cd01879
Antonio Guerra <antonioguerrained@gmail.com>
2021-08-27 15:16:54 +0100

    Heroku setup done

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_132704) do
+ActiveRecord::Schema.define(version: 2021_08_27_135406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "comment"
-    t.bigint "list_id", null: false
     t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
